0866722c92ea46b9fdf47bb3ee0455736a50eade
Maxim <strelok88@mail.ru>
2023-03-06 22:49:03 +0300

    Четвертый коммит

--- crm/migrations/0001_initial.py
+++ crm/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Order',
+            name='Orders',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('order_dt', models.DateTimeField(auto_now=True)),
