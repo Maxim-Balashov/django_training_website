9bd034fa0dc6b30b2c14b05ccbfb7f2189e42c72
Maxim <strelok88@mail.ru>
2023-03-10 01:34:27 +0300

    Седьмой коммит

--- crm/views.py
+++ crm/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from .models import Orders
 from .forms import OrderForms
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
 
 
 def first_page(request):
-    object_list = Orders.objects.all()
-    form = OrderForms()
-    return render(request, './index.html', {'object_list': object_list,
-                                            'form': form})
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    dict_obj = {'slider_list': slider_list,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table}
+    return render(request, './index.html', dict_obj)
 
 
 def thanks_page(request):
--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crm.apps.CrmConfig',
-    # 'crm',
+    'cms.apps.CmsConfig',
+    'price.apps.PriceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
